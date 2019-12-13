db30080222e65740c82152bcb74b029b9cc62c9c
juanfrandm98 <juanfrandm98@gmail.com>
2019-12-13 17:41:37 +0100

    Bastantes avances (checkin, inicio movimiento, tutoria...)

---
+++ practica3/src/practica3/DireccionBuscador.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica3;
+
+/**
+ *
+ * @author juanfrandm98
+ */
+public enum DireccionBuscador {
+    NORTE,
+    SUR,
+    ESTE,
+    OESTE
+}
