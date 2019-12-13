882a777e2418438d336b5a5d5942b4e38226f1fc
juanfrandm98 <juanfrandm98@gmail.com>
2019-12-13 17:47:30 +0100

    Bastantes avances (checkin, inicio movimiento, tutoria...)

--- practica3/src/practica3/DroneBuscador.java
+++ practica3/src/practica3/DroneBuscador.java
@@ -5,6 +5,7 @@
  */
 package practica3;
 
+import com.eclipsesource.json.JsonObject;
 import es.upv.dsic.gti_ia.core.AgentID;
 
 /**
@@ -14,12 +15,91 @@ import es.upv.dsic.gti_ia.core.AgentID;
  */
 public class DroneBuscador extends AbstractDrone {
     
-    public DroneBuscador(AgentID aid) throws Exception {
-        super(aid);
+    // Direccion hacia la que se dirige el drone
+    private DireccionBuscador direccion;
+
+    
+    /**
+      * 
+      * Constructor de la clase
+      * 
+      * @param aid Tipo de drone buscador
+      * @param mapa Nombre del mapa en el que buscar
+      * @throws Exception Excepcion a lanzar
+      * @Author Juan Francisco Diaz Moreno, Ana Rodriguez Duran
+      */
+    public DroneBuscador(AgentID aid, String mapa ) throws Exception {
+        super( aid, mapa );
     }
     
     @Override
     public void actuacion() {
+        
+        seleccionPrimerObjetivo();
+        
+        while( getAlemanesEncontrados() < getAlemanesTotales() ) {
+            
+            actualizarPercepcion();
+            
+            
+            if( alemanVisualizado() ) {
+                notificarPosicion();
+            }
+            
+            mover();
+            
+        }
+        
+    }
+    
+    private void notificarPosicion() {
+        
+        JsonObject coordenadas = calcularPosicion( );
+        
+    }
+
+    private void seleccionPrimerObjetivo() {
+        
+        setObjetivox ( getPosx() );
+        
+        switch( super.getRolname() ) {
+            case "FLY":
+                setObjetivoy( getVisibilidad() );
+                direccion = DireccionBuscador.SUR;
+                break;
+            case "SPARROW":
+                setObjetivoy( getMap().getHeight() - super.getVisibilidad() );
+                direccion = DireccionBuscador.NORTE;
+                break;
+            case "HAWK":
+                setObjetivoy( getMap().getHeight() - super.getVisibilidad() );
+                direccion = DireccionBuscador.NORTE;
+                break;
+        }
+        
+    }
+
+    @Override
+    public String calcularSiguienteMovimiento() {
+        
+        String movimiento;
+        
+        switch( direccion ) {
+            case NORTE:
+                movimiento = "moveN";
+                break;
+            case ESTE:
+                movimiento = "moveE";
+                break;
+            case OESTE:
+                movimiento = "moveW";
+                break;
+            default:
+                movimiento = "moveS";
+                break;
+        }
+        
+        return movimiento;
         
     }
 }
